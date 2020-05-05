fde095fbd8c7b3fa6eb68c12f7449ed19d5b2cc7
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-05 11:02:28 +0530

    Refactored to work with one EmpWageBuilder

--- UserLogic.java
+++ UserLogic.java
@@ -7,18 +7,25 @@ public class UserLogic {
     public static void main(String[] args) {
         Employee raghu = new Employee("raghu", "raghu01");
         Employee eswar = new Employee("eswar", "eswar02");
-        Employee ramu = new Employee("ramu", "ramu03");
-        ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(raghu);
-        employees.add(eswar);
-        employees.add(ramu);
-        Company company = new Company("my company", new EmployeeWageBuilder(), employees);
-        Map<String, Integer> wages = company.calculateEmployeesWage();
-        Iterator<String> iterator = wages.keySet().iterator();
-        while (iterator.hasNext()) {
-            String key = iterator.next();
-            System.out.println(key+"       "+wages.get(key));
+        Employee ramu = new Employee("ram", "ram01");
+        Employee krishna = new Employee("krishna", "krishna02");
+        ArrayList<Employee> D_martEmployees = new ArrayList<>();
+        ArrayList<Employee> relianceEmployees = new ArrayList<>();
+        EmployeeWageBuilder wageBuilder = new EmployeeWageBuilder();
+        D_martEmployees.add(raghu);
+        D_martEmployees.add(eswar);
+        relianceEmployees.add(ramu);
+        relianceEmployees.add(krishna);
+        Company D_Mart = new Company("D mart", wageBuilder, D_martEmployees);
+        Company reliance = new Company("Reliance", wageBuilder, relianceEmployees);
+        Map<String, Integer> D_MartWages = D_Mart.calculateEmployeesWage();
+        Map<String, Integer> relianceWages = reliance.calculateEmployeesWage();
+        System.out.println("D mart wages");
+        for (String key : D_MartWages.keySet()) {
+            System.out.println(key + "       " + D_MartWages.get(key));
+        }System.out.println("Reliance wages");
+        for (String key : relianceWages.keySet()) {
+            System.out.println(key + "       " + relianceWages.get(key));
         }
-        System.out.println(raghu.getCurrentMonthWage()+"  "+eswar.getCurrentMonthWage()+"  "+ramu.getCurrentMonthWage());
     }
 }
