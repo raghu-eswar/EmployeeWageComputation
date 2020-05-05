aee41325755b6d890117e30c45c52a69d6fdc0d6
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-05 09:08:32 +0530

    Added feature to check attendance

--- WageComputation.java
+++ WageComputation.java
@@ -1,3 +1,18 @@
+import java.util.Random;
+
 public class WageComputation {
     
+    static Random random = new Random();
+
+    public boolean isEmployeePresent() {
+        return (random.nextInt(3) > 0);
+    }
+    public static void main(String[] args) {
+        WageComputation WageComputation = new WageComputation();
+        boolean isPresent = WageComputation.isEmployeePresent();
+        if (isPresent)
+            System.out.println("employee was present");
+        else
+            System.out.println("employee was not present");
+    }
 }
