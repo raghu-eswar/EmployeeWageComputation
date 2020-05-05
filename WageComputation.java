04c3b7cdaa1c0f11762037b276d052fcb9a8d8b5
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-05 10:09:21 +0530

    Added class method to calculate wages

--- WageComputation.java
+++ WageComputation.java
@@ -3,39 +3,27 @@ import java.util.Random;
 public class WageComputation {
     
     static Random random = new Random();
-    private int wagePerHour;
-    private int workHoursPerDay;
-    private boolean fullTimeEmployee;
+    private static boolean isEmployeePresent = (random.nextInt(3) > 0 );
+    private static int wagePerHour = 20;
+    private static boolean fullTimeEmployee = random.nextBoolean();
+    private static int workHoursPerDay = (fullTimeEmployee)? 8 : 4;  
 
-    public WageComputation(int wagePerHour){
-        this.wagePerHour = wagePerHour;
-        this.fullTimeEmployee = random.nextBoolean();
-        this.workHoursPerDay = (this.fullTimeEmployee)? 8 : 4;
+    public static int calculateDailyWage() {
+        return workHoursPerDay * wagePerHour;
     }
 
-    public int getWorkHoursPerDay() {
-        return this.workHoursPerDay;
-    }
-
-    public boolean isEmployeePresent() {
-        return (random.nextInt(3) > 0);
-    }
-
-    public int calculateDailyWage() {
-        return this.workHoursPerDay * this.wagePerHour;
+    public static int calculateMonthlyWage() {
+        int monthlyWage = 0;
+        int currentMonthWorkHours = 0;
+        for (int i=1; i<=20 && currentMonthWorkHours < 100; i++) {
+            isEmployeePresent = (random.nextInt(3) > 0 );
+            if (isEmployeePresent) monthlyWage+= calculateDailyWage();
+        }
+        return monthlyWage;
     }
 
     public static void main(String[] args) {
-        int monthlyWage = 0;
-        int currentMonthWorkHours = 0;
-        WageComputation wageComputation = new WageComputation(20);
-
-        for (int i=1; i<=20 && currentMonthWorkHours < 100; i++) {
-            if (wageComputation.isEmployeePresent()) {
-                monthlyWage+= wageComputation.calculateDailyWage();
-                currentMonthWorkHours+= wageComputation.getWorkHoursPerDay();
-            }
-        }
-        System.out.println("employee monthly wage of employee : "+monthlyWage);
+        int monthlyWage = WageComputation.calculateMonthlyWage();
+        System.out.println("employee monthly wage : "+monthlyWage);
     }
 }
