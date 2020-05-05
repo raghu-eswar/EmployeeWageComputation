6fec0e4b2396b7a857a7d76f7452086ff8d3f727
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-05 09:26:05 +0530

    Added feature to calculate daily wage

--- WageComputation.java
+++ WageComputation.java
@@ -3,15 +3,27 @@ import java.util.Random;
 public class WageComputation {
     
     static Random random = new Random();
+    private int wagePerHour;
+    private int workHoursPerDay;
+
+    public WageComputation(int wagePerHour, int workHoursPerDay) {
+        this.wagePerHour = wagePerHour;
+        this.workHoursPerDay = workHoursPerDay;
+    }
 
     public boolean isEmployeePresent() {
         return (random.nextInt(3) > 0);
     }
+
+    public int calculateDailyWage() {
+        return this.workHoursPerDay * this.wagePerHour;
+    }
+
     public static void main(String[] args) {
-        WageComputation WageComputation = new WageComputation();
-        boolean isPresent = WageComputation.isEmployeePresent();
-        if (isPresent)
-            System.out.println("employee was present");
+        WageComputation wageComputation = new WageComputation(20, 8);
+        boolean isPresent = wageComputation.isEmployeePresent();
+        if(isPresent) 
+            System.out.println("daily wage of employe : "+wageComputation.calculateDailyWage());
         else
             System.out.println("employee was not present");
     }
